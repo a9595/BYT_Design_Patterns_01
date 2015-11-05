ba93e7d52c1760bc829d3ecb19e7c76a6c6a85d9
Andrii Kovalchuk <tieorange@gmail.com>
2015-11-05 12:36:25 +0100

    init bodyParts

---
+++ BYT_Design_Patterns_01/Eyes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYT_Design_Patterns_01
+{
+    class Eyes : BodyPart
+    {
+        public string Picture { get; private set; } = string.Empty;
+        public Eyes(Brain brain) : base(brain) { }
+
+        public void SeeSomething()
+        {
+            Console.WriteLine("Enter what you see: ");
+            Picture = Console.ReadLine();
+
+            Changed();
+        }
+
+        
+    }
+}
